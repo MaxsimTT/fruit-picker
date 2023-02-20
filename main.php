5ae4add84d4a7b3536b895bbb2d23ee92d3a76c1
Max <egorovcotmax@gmail.com>
2023-02-20 17:28:18 +0400

    added output in template

--- main.php
+++ main.php
@@ -1,9 +1,13 @@
 <?php
 
 use classes\Garden;
+use classes\ArrayFilling;
 
 require_once __DIR__ . '\helpers\funcs.php';
 
 spl_autoload_register('autoloader');
+
+$garden = new Garden(new ArrayFilling());
+$data = picker($garden);
 
 include __DIR__ . ('\views\info_fruits.php');
