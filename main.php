20ea7da865afe2fa60ed9efb081e23b0f8db4fd6
Max <egorovcotmax@gmail.com>
2023-02-20 17:36:16 +0400

    created adding trees into garden using database && added output in template from data database

--- main.php
+++ main.php
@@ -1,13 +1,16 @@
 <?php
 
 use classes\Garden;
+use classes\Db;
+use classes\DbFilling;
 use classes\ArrayFilling;
 
 require_once __DIR__ . '\helpers\funcs.php';
 
 spl_autoload_register('autoloader');
 
-$garden = new Garden(new ArrayFilling());
+// $garden = new Garden(new ArrayFilling());
+$garden = new Garden(new DbFilling());
 $data = picker($garden);
 
 include __DIR__ . ('\views\info_fruits.php');
